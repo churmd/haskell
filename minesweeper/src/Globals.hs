05c5c287b9d48fe9fef6e490930e01302e2ad40d
churmd <churmdaniel@gmail.com>
2017-07-24 16:57:48 +0100

    alterd globals

--- minesweeper/src/Globals.hs
+++ minesweeper/src/Globals.hs
@@ -1,16 +1,19 @@
 module Globals where
 
+  border :: Float
+  border = 100
+
   getScreenWidth :: Float
-  getScreenWidth = 600
+  getScreenWidth = 600 - border
 
   getScreenHeight :: Float
   getScreenHeight = 600
 
   getWidth :: Float
-  getWidth = getScreenWidth - 50
+  getWidth = getScreenWidth - 4
 
   getHeight :: Float
-  getHeight = getScreenHeight - 50 
+  getHeight = getScreenHeight - border
 
   getBoardSize :: Int
   getBoardSize = 16
