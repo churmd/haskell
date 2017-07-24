8d99878dc2edcc2fa99dd870ea857216bc4fb57e
churmd <churmdaniel@gmail.com>
2017-07-24 20:01:45 +0100

    Revert "Undid all changes from adding time"
    This reverts commit 65536068915c7db139e811438ef34ce6d8f7f813.

--- minesweeper/src/UserInput.hs
+++ minesweeper/src/UserInput.hs
@@ -3,34 +3,40 @@ module UserInput where
   import Board
   import Globals
   import System.Random
+  import Data.Time
 
   handler :: Event -> Board -> IO (Board)
-  handler (EventKey (MouseButton LeftButton) Up _ (x, y)) b@(Board Win _ _ _) =
+  handler (EventKey (MouseButton LeftButton) Up _ (x, y)) b@(Board Win _ _ _ _) =
     return b
-  handler (EventKey (MouseButton LeftButton) Up _ (x, y)) b@(Board Loss _ _ _) =
+  handler (EventKey (MouseButton LeftButton) Up _ (x, y)) b@(Board Loss _ _ _ _) =
     return b
   handler (EventKey (MouseButton LeftButton) Up _ (x, y)) b = do
     let c = getBoardCoord (x,y) (getWidth) (getHeight) b
     if cellInRange b c then
-      return (revealCell b c)
+      let started = startGame b in
+      return (revealCell started c)
     else
       return b
-  handler (EventKey (Char 'r') Up _ _) (Board st sz nm cells) = do
+  handler (EventKey (Char 'r') Up _ _) (Board st sz nm t cells) = do
     g <- newStdGen
-    return (makeBoard sz nm g)
+    time <- getCurrentTime
+    return (makeBoard sz nm time g)
   handler (EventKey (Char 'e') Up _ _) b = do
     g <- newStdGen
-    return (makeBoard easyBoard easyMines g)
+    time <- getCurrentTime
+    return (makeBoard easyBoard easyMines time g)
   handler (EventKey (Char 'm') Up _ _) b = do
     g <- newStdGen
-    return (makeBoard medBoard medMines g)
+    time <- getCurrentTime
+    return (makeBoard medBoard medMines time g)
   handler (EventKey (Char 'h') Up _ _) b = do
     g <- newStdGen
-    return (makeBoard hardBoard hardMines g)
+    time <- getCurrentTime
+    return (makeBoard hardBoard hardMines time g)
   handler _ b = return b
 
   getBoardCoord :: (Float, Float) -> Float -> Float -> Board -> Coord
-  getBoardCoord (x,y) width height (Board st sz nm cells) =
+  getBoardCoord (x,y) width height (Board st sz nm t cells) =
     let cellWidth = width / (fromIntegral sz) in
     let cellHeight = height / (fromIntegral sz) in
     let yOffset = (height/2) in
