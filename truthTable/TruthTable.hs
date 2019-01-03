c1266e3393536091e542f161f6e508ef85496b9a
churmd <churmdaniel@gmail.com>
2019-01-03 21:34:06 +0000

    Implemented functions to subsitute varibales in expressions

--- truthTable/TruthTable.hs
+++ truthTable/TruthTable.hs
@@ -1,3 +1,35 @@
 module TruthTable where
+  import LogicExpressions
 
-  data TruthTable = 
+  type PlaceHolderExpr = Expr String
+  type VarTable = [(String, Bool)]
+  type ExprCombination = (PlaceHolderExpr, VarTable)
+
+  extractVarNames :: PlaceHolderExpr -> [String]
+  extractVarNames (Var s) = [s]
+  extractVarNames (Not s) = extractVarNames s
+  extractVarNames (And a b) = extractVarNames a ++ extractVarNames b
+  extractVarNames (Or a b) = extractVarNames a ++ extractVarNames b
+  extractVarNames (Xor a b) = extractVarNames a ++ extractVarNames b
+
+  allPropositions :: [a] -> [[(a, Bool)]]
+  allPropositions xs = sequence $ map (\x -> [(x, True), (x, False)]) xs
+
+  lookUp :: (Eq a) => a -> [(a, b)] -> Maybe b
+  lookUp k [] = Nothing
+  lookUp k ((key, value):xs) | k == key = Just value
+                             | otherwise = lookUp k xs
+
+  substituteVars :: VarTable -> PlaceHolderExpr -> Maybe LogicExpr
+  substituteVars vt (Var a) = lookUp a vt >>= (\b ->  return (Var b))
+  substituteVars vt (Not a) = substituteVars vt a >>= \logExpr ->
+                              return (Not logExpr)
+  substituteVars vt (And a b) = substituteVars vt a >>= \logExprA ->
+                                substituteVars vt b >>= \logExprB ->
+                                return (And logExprA logExprB)
+  substituteVars vt (Or a b) = substituteVars vt a >>= \logExprA ->
+                                substituteVars vt b >>= \logExprB ->
+                                return (Or logExprA logExprB)
+  substituteVars vt (Xor a b) = substituteVars vt a >>= \logExprA ->
+                                substituteVars vt b >>= \logExprB ->
+                                return (Xor logExprA logExprB)  
