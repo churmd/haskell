a89d4c77d12c0eeea4aed02fcb123344bb4b6c66
churmd <churmdaniel@gmail.com>
2019-01-14 21:06:28 +0000

    Created an evaluate function for Expr String in truth table.

--- truthTable/LogicExpressions.hs
+++ truthTable/LogicExpressions.hs
@@ -1,43 +1,43 @@
 module LogicExpressions where
 
-  data Expr a =
-    Var a |
-    Not (Expr a) |
-    And (Expr a) (Expr a) |
-    Or (Expr a) (Expr a) |
-    Xor (Expr a) (Expr a)
-    deriving (Eq)
+data Expr a =
+  Var a |
+  Not (Expr a) |
+  And (Expr a) (Expr a) |
+  Or (Expr a) (Expr a) |
+  Xor (Expr a) (Expr a)
+  deriving (Eq)
 
-  instance Show (a) => Show (Expr a) where
-    show (Var a) = show a
-    show (Not a) =  "Not " ++ show a
-    show (And a b) = "(" ++ show a ++ ") And (" ++ show b ++ ")"
-    show (Or a b) = "(" ++ show a ++ ") Or (" ++ show b ++ ")"
-    show (Xor a b) = "(" ++ show a ++ ") Xor (" ++ show b ++ ")"
+instance Show (a) => Show (Expr a) where
+  show (Var a) = show a
+  show (Not a) =  "Not " ++ show a
+  show (And a b) = "(" ++ show a ++ ") And (" ++ show b ++ ")"
+  show (Or a b) = "(" ++ show a ++ ") Or (" ++ show b ++ ")"
+  show (Xor a b) = "(" ++ show a ++ ") Xor (" ++ show b ++ ")"
 
-  type LogicExpr = Expr Bool
+type LogicExpr = Expr Bool
 
-  eval :: LogicExpr -> Bool
-  eval (Var True) = True
-  eval (Var False) = False
-  eval (Not a) = evalNot (eval a)
-  eval (And a b) = evalAnd (eval a) (eval b)
-  eval (Or a b ) = evalOr (eval a) (eval b)
-  eval (Xor a b ) = evalXor (eval a) (eval b)
+eval :: LogicExpr -> Bool
+eval (Var True) = True
+eval (Var False) = False
+eval (Not a) = evalNot (eval a)
+eval (And a b) = evalAnd (eval a) (eval b)
+eval (Or a b ) = evalOr (eval a) (eval b)
+eval (Xor a b ) = evalXor (eval a) (eval b)
 
-  evalNot :: Bool -> Bool
-  evalNot True = False
-  evalNot False = True
+evalNot :: Bool -> Bool
+evalNot True = False
+evalNot False = True
 
-  evalAnd :: Bool -> Bool -> Bool
-  evalAnd True True = True
-  evalAnd _ _ = False
+evalAnd :: Bool -> Bool -> Bool
+evalAnd True True = True
+evalAnd _ _ = False
 
-  evalOr :: Bool -> Bool -> Bool
-  evalOr False False = False
-  evalOr _ _ = True
+evalOr :: Bool -> Bool -> Bool
+evalOr False False = False
+evalOr _ _ = True
 
-  evalXor :: Bool -> Bool -> Bool
-  evalXor True True = False
-  evalXor False False = False
-  evalXor _ _ = True
+evalXor :: Bool -> Bool -> Bool
+evalXor True True = False
+evalXor False False = False
+evalXor _ _ = True
--- truthTable/TruthTable.hs
+++ truthTable/TruthTable.hs
@@ -1,35 +1,53 @@
 module TruthTable where
-  import LogicExpressions
+import LogicExpressions as LE
 
-  type PlaceHolderExpr = Expr String
-  type VarTable = [(String, Bool)]
-  type ExprCombination = (PlaceHolderExpr, VarTable)
+type PlaceHolderExpr = Expr String
+type VarTable = [(String, Bool)]
+type ExprCombination = (VarTable, PlaceHolderExpr)
 
-  extractVarNames :: PlaceHolderExpr -> [String]
-  extractVarNames (Var s) = [s]
-  extractVarNames (Not s) = extractVarNames s
-  extractVarNames (And a b) = extractVarNames a ++ extractVarNames b
-  extractVarNames (Or a b) = extractVarNames a ++ extractVarNames b
-  extractVarNames (Xor a b) = extractVarNames a ++ extractVarNames b
+extractVarNames :: PlaceHolderExpr -> [String]
+extractVarNames (Var s) = [s]
+extractVarNames (Not s) = extractVarNames s
+extractVarNames (And a b) = extractVarNames a ++ extractVarNames b
+extractVarNames (Or a b) = extractVarNames a ++ extractVarNames b
+extractVarNames (Xor a b) = extractVarNames a ++ extractVarNames b
 
-  allPropositions :: [a] -> [[(a, Bool)]]
-  allPropositions xs = sequence $ map (\x -> [(x, True), (x, False)]) xs
+allPropositions :: [a] -> [[(a, Bool)]]
+allPropositions xs = sequence $ map (\x -> [(x, True), (x, False)]) xs
 
-  lookUp :: (Eq a) => a -> [(a, b)] -> Maybe b
-  lookUp k [] = Nothing
-  lookUp k ((key, value):xs) | k == key = Just value
-                             | otherwise = lookUp k xs
+lookUp :: (Eq a) => a -> [(a, b)] -> Maybe b
+lookUp k [] = Nothing
+lookUp k ((key, value):xs) | k == key = Just value
+                           | otherwise = lookUp k xs
 
-  substituteVars :: VarTable -> PlaceHolderExpr -> Maybe LogicExpr
-  substituteVars vt (Var a) = lookUp a vt >>= (\b ->  return (Var b))
-  substituteVars vt (Not a) = substituteVars vt a >>= \logExpr ->
-                              return (Not logExpr)
-  substituteVars vt (And a b) = substituteVars vt a >>= \logExprA ->
-                                substituteVars vt b >>= \logExprB ->
-                                return (And logExprA logExprB)
-  substituteVars vt (Or a b) = substituteVars vt a >>= \logExprA ->
-                                substituteVars vt b >>= \logExprB ->
-                                return (Or logExprA logExprB)
-  substituteVars vt (Xor a b) = substituteVars vt a >>= \logExprA ->
-                                substituteVars vt b >>= \logExprB ->
-                                return (Xor logExprA logExprB)  
+substituteVars :: ExprCombination -> Maybe LogicExpr
+substituteVars (vt, (Var a)) =
+  lookUp a vt >>= (\b ->  return (Var b))
+substituteVars (vt, (Not a)) =
+  substituteVars (vt, a) >>= \logExpr ->
+  return (Not logExpr)
+substituteVars (vt, (And a b)) =
+  substituteVars (vt, a) >>= \logExprA ->
+  substituteVars (vt, b) >>= \logExprB ->
+  return (And logExprA logExprB)
+substituteVars (vt, (Or a b)) =
+  substituteVars (vt, a) >>= \logExprA ->
+  substituteVars (vt, b) >>= \logExprB ->
+  return (Or logExprA logExprB)
+substituteVars (vt, (Xor a b)) =
+  substituteVars (vt, a) >>= \logExprA ->
+  substituteVars (vt, b) >>= \logExprB ->
+  return (Xor logExprA logExprB)
+
+eval :: PlaceHolderExpr -> [(VarTable, Maybe Bool)]
+eval expr = zip varTables (evalHelper varTables expr)
+  where
+    varTables = allPropositions $ extractVarNames expr
+
+evalHelper :: [VarTable] -> PlaceHolderExpr -> [Maybe Bool]
+evalHelper [] _ = []
+evalHelper (vt:vts) expr = [evalHelper2 (vt, expr)] ++ (evalHelper vts expr)
+
+evalHelper2 :: ExprCombination -> Maybe Bool
+evalHelper2 exprCom = substituteVars exprCom >>= \lExpr ->
+                      return (LE.eval lExpr)
