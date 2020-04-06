91237c49fca01e5d0786307a7dc802991e21ab82
churmd <churmdaniel@gmail.com>
2020-04-06 21:28:28 +0100

    Implemented a few more list functions

--- utils/ListUtils.hs
+++ utils/ListUtils.hs
@@ -1,19 +1,81 @@
 module ListUtils (
+  append,
+  prepend,
+  len,
+  rev,
+  equal,
   isPrefix,
+  isSuffix,
   indexOfSubList,
+  contains,
+  replaceFirst,
+  replaceAll
 ) where
+
+{-|
+  Add an element to the beginning of a list.
+-}
+prepend :: a -> [a] -> [a]
+prepend x ys = x : ys
+
+{-|
+  Add an element to the end of a list.
+-}
+append :: [a] -> a -> [a]
+append xs y = xs ++ [y]
+
+{-|
+  Returns the length of the list.
+-}
+len :: [a] -> Integer
+len [] = 0
+len (x : xs) = 1 + (len xs)
+
+{-|
+  Reverses the list.
+-}
+rev :: [a] -> [a]
+rev [] = []
+rev (x : xs) = append (rev xs) x
+
+{-|
+  Returns True if the lists are equal, otherwise False.
+-}
+equal :: (Eq a) => [a] -> [a] -> Bool
+equal [] [] = True
+equal [] _ = False
+equal _ [] = False
+equal (x : xs) (y : ys) =
+  if x == y
+    then equal xs ys
+    else False
 
 {-|
   Returns True if the second list is the beginning of the first,
   otherwise False.
 -}
 isPrefix :: (Eq a) => [a] -> [a] -> Bool
-isPrefix _ [] = False
+isPrefix _ [] = True
 isPrefix [] (y:ys) = False
 isPrefix (x:xs) (y:ys) =
   if x == y
     then isPrefix xs ys
     else False
+
+{-|
+  Returns True if the second list is the end of the first,
+  otherwise False.
+-}
+isSuffix :: (Eq a) => [a] -> [a] -> Bool
+isSuffix _ [] = True
+isSuffix [] _ = False
+isSuffix l1 l2
+  | lenL1 < lenL2 = False
+  | lenL1 == lenL2 = equal l1 l2
+  | otherwise = isSuffix (tail l1) l2
+    where
+      lenL1 = len l1
+      lenL2 = len l2
 
 {-|
   Searches the main list for the first occurence of the sub list.
@@ -30,3 +92,33 @@ indexOfSubListHelper mainList@(x:xs) subList acc =
   if isPrefix mainList subList
     then acc
     else indexOfSubListHelper xs subList (acc + 1)
+
+{-|
+  Returns True if the first list contains the second list within it,
+  otherwise false.
+-}
+contains :: (Eq a) => [a] -> [a] -> Bool
+contains mainList subList = indexOfSubList mainList subList /= -1
+
+{-|
+  Replaces the first occurence of an element with another element.
+  If no matching element is found, the original list is returned.
+  e.g. replaceFirst [1,1,2,3] 1 5 = [5,1,2,3]
+-}
+replaceFirst :: (Eq a) => [a] -> a -> a -> [a]
+replaceFirst [] _ _ = []
+replaceFirst (x : xs) toReplace replacement =
+  if x == toReplace
+    then replacement : xs
+    else x : (replaceFirst xs toReplace replacement)
+
+{-|
+  Replaces the all occurences of an element with another element.
+  e.g. replaceFirst [1,1,2,3] 1 5 = [5,5,2,3]
+-}
+replaceAll :: (Eq a) => [a] -> a -> a -> [a]
+replaceAll [] _ _ = []
+replaceAll (x : xs) toReplace replacement =
+  if x == toReplace
+    then replacement : (replaceAll xs toReplace replacement)
+    else x : (replaceAll xs toReplace replacement)
